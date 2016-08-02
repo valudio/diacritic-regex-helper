1825a1f151e650833b67ca511b0155b9d5eb15fc
Roberto Huertas <roberto.huertas@outlook.com>
2016-08-02 20:54:03 +0200

    escaping conflicting characters

--- index.js
+++ index.js
@@ -11,12 +11,16 @@ var chars = [
   'TtẗŢţȾⱦŦŧŤťṪṫṬṭ',
   'UuÚúÙùÛûÜüŲųɄʉǓǔỤụŪūŨũ',
   'YyÝýỲỳŶŷŸÿɎɏẎẏỴỵȲȳỸỹ',
-  'ZzŹźẐẑƵƶŽžŻżẒẓ',
+  'ZzŹźẐẑƵƶŽžŻżẒẓ'
 ];
+
+function escapeRegExp(string){
+  return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
+}
 
 var diacritic = function(text){
   if(!text) return '';
-  var result = text;
+  var result = escapeRegExp(text);
   for (var char in chars) { // eslint-disable-line
     result = result.replace(new RegExp('[' + chars[char] + ']', 'gi'), '[' + chars[char] + ']');
   }
